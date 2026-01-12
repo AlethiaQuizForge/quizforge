70ca8bd2157fd0aa034f61d3b96b48e0c27ad9e5
AlethiaQuizForge <5twdz2gr49@privaterelay.appleid.com>
2026-01-12 18:08:23 +0100

    Add Capacitor iOS setup

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.quizforgeapp.app',
+  appName: 'QuizForge',
+  webDir: 'out'
+};
+
+export default config;
